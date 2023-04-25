124cc630bec007b4d10a42286f527db384019eed
voldo <filipecavoldo@outlook.com>
2023-04-25 20:06:59 -0300

    adicionado javascript do formulario

---
+++ faleconosco.js
@@ -0,0 +1,31 @@
+function verifyForm(){
+    var enviar = true;
+
+    var nome = document.form1.txtNome.value;
+    if (nome.length == 0){
+        enviar = false;
+        document.querySelector("#erroNome").textContent =
+            "\nPreencha com um nome";
+    }
+    var idade = document.form1.txtIdade.value;
+    if (idade.length == 0){
+        enviar = false;
+        document.querySelector("#erroIdade").textContent =
+            "\nPreencha com uma idade";
+    }
+    var email = document.form1.txtEmail.value;
+    if (email.length == 0){
+        enviar = false;
+        document.querySelector("#erroEmail").textContent =
+            "\nPreencha com um email";
+    }
+    var feedback = document.form1.txtFeedback.value;
+    if (feedback.length == 0){
+        enviar = false;
+        document.querySelector("#erroFeedback").textContent =
+            "\nPreencha com um feedback";
+    }
+    if (enviar){
+        document.form1.submit;
+    }
+}
